99b253180937a977fc12655d583289ba72ff35fa
Jonghee Son <zzongaone@gmail.com>
2024-01-09 22:32:09 +0900

    Cyclone V requires hello_world_small since on chip memory size is restricted

---
+++ software/hello_world_small/obj/default/hello_world_small.d
@@ -0,0 +1,3 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ c:\users\jongh\documents\repo\cyclone_v_nios_ii\software\hello_world_small_bsp\hal\inc\sys\alt_stdio.h
+c:\users\jongh\documents\repo\cyclone_v_nios_ii\software\hello_world_small_bsp\hal\inc\sys\alt_stdio.h:
